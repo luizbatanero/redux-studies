fe5d74e1bb351f419579be831b5544f7e70b8eb6
Luiz Batanero <luizbatanero@gmail.com>
2019-07-06 21:43:44 -0300

    Persist todos state to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,24 @@
 import { createStore } from 'redux';
+import { throttle } from 'lodash';
+
 import reducers from '../reducers';
+import { loadState, saveState } from '../localStorage';
+
+const persistedState = loadState();
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      todos: store.getState().todos,
+    });
+  }, 1000)
 );
 
 export default store;
